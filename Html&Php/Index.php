4fd7071cd8b05e7d77a2cba3ad2be022a816101f
Supremo502 <115429630+Supremo502@users.noreply.github.com>
2023-01-19 20:48:39 -0500

    Add files via upload

---
+++ Html&Php/Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Nose que poner</title>
+</head>
+<body>
+	<h1>Aprendindo php para pocketmine y crear webs dinamicos</h1>`
+<?php
+$name = "RonyMartiez";
+
+$languages = 4;
+
+echo $name;
+
+"\n"
+
+echo "Seguiremos aprendiendo php";
+
+?>
+</body>
+</html>
